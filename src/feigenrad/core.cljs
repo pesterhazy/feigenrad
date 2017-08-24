a19d58768be44922eb703a9fb3e2a5a30caa915e
Paulus Esterhazy <pesterhazy@gmail.com>
2017-08-24 12:00:28 -0300

    WIP

--- src/feigenrad/core.cljs
+++ src/feigenrad/core.cljs
@@ -3,15 +3,11 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/feigenrad/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(prn [:foo])
+
 
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
