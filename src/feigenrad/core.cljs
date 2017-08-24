f9a5cb3db757c1ede160c63d646397d295046e24
Paulus Esterhazy <pesterhazy@gmail.com>
2017-08-24 14:24:37 -0300

    WIP

--- src/feigenrad/core.cljs
+++ src/feigenrad/core.cljs
@@ -1,13 +1,36 @@
 (ns feigenrad.core
-    (:require ))
+  (:require [goog.object :as gobj]))
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over-written on reload
-
 (prn [:foo])
 
+(defn my-component [props context updater]
+  (cljs.core/this-as this
+    (js/React.Component.call this props context updater)
+    ;; anything else you want to set-up. use goog.object/set on this
+    (gobj/extend this #js{"render"
+                          (fn []
+                            (js/React.createElement "div" #js{} #js["Welcome!!"]))})
+    this))
 
+(gobj/extend
+    (.. my-component -prototype)
+  js/React.Component.prototype)
+
+;; cljs-specific properties on constructor
+;; just so (prn my-component) works properly, not actually required
+(set! (.-cljs$lang$type my-component) true)
+(set! (.-cljs$lang$ctorStr my-component) "MyComponent")
+(set! (.-cljs$lang$ctorPrWriter my-component)
+      (fn [this writer opt]
+        (cljs.core/-write writer "MyComponent")))
+(set! (.. my-component -prototype -constructor) my-component)
+
+(defn run []
+  (js/ReactDOM.render (js/React.createElement my-component) (js/document.getElementById "app")))
+
+(run)
 
 (defn on-js-reload []
   )
